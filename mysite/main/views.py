bb6654ddff6f7d2b378212020d19877edaf92cde
HariSupikssa <131352388+HariSupikssa@users.noreply.github.com>
2024-12-10 17:35:59 +0530

    Initial commit

---
+++ mysite/main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(response):
+    return HttpResponse("<h1>This is a view.</h1>")
+
+def v1(response):
+    return HttpResponse("<h1>This is view1.</h1>")
